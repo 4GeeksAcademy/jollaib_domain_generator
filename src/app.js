9c99e44020f3b32c8a80f9eb1aa253e597f8e362
jollaib <56800825+jollaib@users.noreply.github.com>
2024-02-01 18:31:59 +0000

    Errores subsanados

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
 let pronouns = ["the", "her"];
 let adjs = ["extraordinary", "awesome"];
 let nouns = ["caries", "team"];
